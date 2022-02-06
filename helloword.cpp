dc4bc5e759b7ab1df97ff39ee2561c55cc0adbd4
Tienanh01 <vutienanhg@gmail.com>
2022-02-06 22:39:11 +0700

    this is my code please check it in tomorrow

---
+++ helloword.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+	printf("Bai hoc c dau tien");
+	
+//	 Chuong trinh nhap va in ra man hinh gia tri bien 
+int i;
+printf("\nNhap vao 1 so : " );
+scanf("%d",&i);
+printf("So ban vua nhap la : %d\n",i);
+//getchar();
+
+// Chuong trinh nhap vao 2 so nguyen duong va in ra tong
+int a, b;
+printf("Nhap so nguyen a : ");
+scanf("%d",&a);
+printf("Nhap vao so nguyen b : ");
+scanf("%d",&b);
+int tong ;
+tong = a+b;
+printf("Tong cua 2 so nguyen a va b la : %d\n",tong);
+printf("Tong cua %d va %d la %d",a,b,a+b);
+
+// Chuong trinh in ra dien tich hinh tron
+
+#define PI 3.14       //  dinh nghia gia tri cua pi la 3.14
+float R;
+printf("\nNhap vao ban kinh hinh tron : ");
+scanf("%f",&R);
+printf("Dien tich hinh tron la : %.2f\n",PI*R*R);
+
+
+
+}
